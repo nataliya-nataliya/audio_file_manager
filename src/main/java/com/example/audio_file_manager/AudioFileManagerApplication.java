b4c0860bf7824f311af91687401380cf62ce71cd
nataliya-nataliya <nataliya.chest@gmail.com>
2024-05-29 08:26:17 +0100

    git init

---
+++ src/main/java/com/example/audio_file_manager/AudioFileManagerApplication.java
@@ -0,0 +1,13 @@
+package com.example.audio_file_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AudioFileManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AudioFileManagerApplication.class, args);
+	}
+
+}
