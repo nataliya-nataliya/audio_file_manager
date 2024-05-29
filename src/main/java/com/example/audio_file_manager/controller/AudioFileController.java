305856ee236ca3b2b35ece7e4fb45769417038f9
nataliya-nataliya <nataliya.chest@gmail.com>
2024-05-29 15:38:18 +0100

    add swagger

--- src/main/java/com/example/audio_file_manager/controller/AudioFileController.java
+++ src/main/java/com/example/audio_file_manager/controller/AudioFileController.java
@@ -4,9 +4,16 @@ import com.example.audio_file_manager.dto.request.SavingAudioFileRequestDto;
 import com.example.audio_file_manager.dto.response.SavingAudioFileResponseDto;
 import com.example.audio_file_manager.exception.NotSaveFileException;
 import com.example.audio_file_manager.service.AudioFileService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,8 +30,15 @@ import java.io.IOException;
 public class AudioFileController {
     private final AudioFileService audioFileService;
 
-    @PostMapping()
-    public ResponseEntity<SavingAudioFileResponseDto> saveAudioFile(@RequestParam MultipartFile file) {
+
+    @Operation(summary = "Save audio file")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Audio file is saved"),
+            @ApiResponse(responseCode = "400", description = "Audio file is not saved", content = @Content)})
+    @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<SavingAudioFileResponseDto> saveAudioFile(@Parameter(description = "input audio file")
+                                                                    @Schema(type = "string", format = "binary")
+                                                                    @RequestParam MultipartFile file) {
         SavingAudioFileResponseDto audioFileResponseDto = new SavingAudioFileResponseDto(true);
         try {
             audioFileService.save(new SavingAudioFileRequestDto(file.getOriginalFilename(), file.getBytes()));
--- src/main/java/com/example/audio_file_manager/handler/FileGlobalExceptionHandler.java
+++ src/main/java/com/example/audio_file_manager/handler/FileGlobalExceptionHandler.java
@@ -6,16 +6,18 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ControllerAdvice
 @Slf4j
 public class FileGlobalExceptionHandler {
     @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ResponseEntity<FileNotSaveData> handlerException(NotSaveFileException exception) {
         FileNotSaveData data = FileNotSaveData.builder()
                 .info(exception.getMessage()).build();
-        log.error("Exception occurred with HTTP status {}: {}", HttpStatus.INTERNAL_SERVER_ERROR.value(),
+        log.error("Exception occurred with HTTP status {}: {}", HttpStatus.BAD_REQUEST.value(),
                 exception.getLocalizedMessage());
-        return new ResponseEntity<>(data, HttpStatus.INTERNAL_SERVER_ERROR);
+        return new ResponseEntity<>(data, HttpStatus.BAD_REQUEST);
     }
 }
